8a674118c6d233e0e8d63026a9fc29b2cdd571a5
kunal.bhushan@rocketmail.com <ubuntu@ip-172-31-12-155.eu-west-1.compute.internal>
2013-07-10 19:39:48 +0000

    correcter web.js

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var express=require('express');
 var app=express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buff.tostring('utf8'));
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
