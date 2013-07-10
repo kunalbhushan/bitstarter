8afaf9ceed14950a237a63ba189713c0771566b9
kunal.bhushan@rocketmail.com <ubuntu@ip-172-31-12-155.eu-west-1.compute.internal>
2013-07-10 20:25:03 +0000

    donno whats happening

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var express=require('express');
 var app=express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buff.toString());
+  response.send("what the hell");
 });
 
 var port = process.env.PORT || 5000;
