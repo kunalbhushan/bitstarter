091c39237754b903f6467b162477a94f1485e6f7
kunal.bhushan@rocketmail.com <ubuntu@ip-172-31-12-155.eu-west-1.compute.internal>
2013-07-10 20:47:14 +0000

    ll

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var express=require('express');
 var app=express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("what the hell");
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
