453eb9c13d9bf386859938ef86548b0c29b31af8
kunal.bhushan@rocketmail.com <ubuntu@ip-172-31-12-155.eu-west-1.compute.internal>
2013-07-10 20:33:29 +0000

    new heroku

--- web.js
+++ web.js
@@ -1,17 +1,9 @@
-var fs=require('fs');
- buff = new Buffer(fs.readFileSync('../bitstarter/index.html','utf8'));
-
-
-
-
 var express=require('express');
 var app=express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send("what the hell");
+app.get('/',function(request,response){
+response.send('Hello World new');
 });
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port,function(){
+console.log("Listening on " + port);
 });
